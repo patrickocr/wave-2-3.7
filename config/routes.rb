81bf99e05c668f99e8d1624b478648c4199d177d
patrickocr <patrickocr@gmail.com>
2017-02-12 17:17:51 +0000

    4.3 attempt 1

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,10 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#landing_page’
+
+  resources :orders, only: [:index, :show, :create, :destroy]
+
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
